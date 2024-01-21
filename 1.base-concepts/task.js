9a73a31b0a6096f79573efb2b082caf53c95c89b
Darya Vylomova <qwerty6108@gmail.com>
2024-01-21 22:45:01 +0300

    first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,18 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let D = Math.pow(b, 2) - 4 * a * c;
+  if(D < 0)
+    return [];
+  let roots;
+  if(D == 0)
+    roots = (-b + Math.sqrt(D)) / (2 * a);
+  else if(D > 0){
+    let tmp = [];
+    tmp.push((-b + Math.sqrt(D)) / (2 * a));
+    tmp.push((-b - Math.sqrt(D)) / (2 * a));
+    roots = tmp;
+  }
   return arr;
 }
 
