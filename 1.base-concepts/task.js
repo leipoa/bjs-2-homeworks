50ecdcce88f1cdddc3b45057e6a69c39be95b962
Darya Vylomova <qwerty6108@gmail.com>
2024-01-25 22:37:02 +0300

    modified task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,21 +1,30 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  let D = Math.pow(b, 2) - 4 * a * c;
-  if(D < 0)
-    return [];
-  let roots;
-  if(D == 0)
-    roots = (-b + Math.sqrt(D)) / (2 * a);
-  else if(D > 0){
-    let tmp = [];
-    tmp.push((-b + Math.sqrt(D)) / (2 * a));
-    tmp.push((-b - Math.sqrt(D)) / (2 * a));
-    roots = tmp;
-  }
-  return arr;
+
+	let D = Math.pow(b, 2) - 4 * a * c;
+	let arr = [];
+	if (D < 0)
+		return arr;
+	let roots;
+	if (D == 0)
+		roots = (-b + Math.sqrt(D)) / (2 * a);
+	else if (D > 0) {
+		let arr = [];
+		arr.push((-b + Math.sqrt(D)) / (2 * a));
+		arr.push((-b - Math.sqrt(D)) / (2 * a));
+		roots = arr;
+	}
+	return roots;
 }
+console.log(solveEquation(1, 4, 3));
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+	let monthPercent = percent / 12;
+	let creditBody = amount - contribution;
+	let monthPayment = creditBody * (monthPercent + (monthPercent / ((Math.pow(1 + monthPercent), countMonths)) - 1));
+	let sum = Number(contribution + creditBody + monthPayment);
+	console.log(sum.toFixed(2));
 }
+console.log(calculateTotalMortgage(10, 1000, 50000, 12));
