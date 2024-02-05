33f65308417baace1ff11d5917e74201a9876e45
Darya Vylomova <qwerty6108@gmail.com>
2024-02-05 21:49:56 +0300

    modified base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,8 +3,8 @@
 function solveEquation(a, b, c) {
   let D = Math.pow(b, 2) - 4 * a * c;
   let arr = [];
-  if (D < 0) return arr;
-  if (D == 0) arr = (-b + Math.sqrt(D)) / (2 * a);
+  if (D < 0) arr = [];
+  if (D == 0) arr = -b / (2 * a);
   else if (D > 0) {
     let roots = [];
     roots.push((-b + Math.sqrt(D)) / (2 * a));
@@ -15,12 +15,11 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let monthPercent = percent / 12;
+  let monthPercent = percent / 12 / 100;
   let creditBody = amount - contribution;
   let monthPayment =
     creditBody *
-    (monthPercent + (monthPercent / (1 + monthPercent) ** countMonths - 1));
-  let sum = contribution + creditBody + monthPayment * countMonths;
+    (monthPercent + monthPercent / ((1 + monthPercent) ** countMonths - 1));
+  let sum = monthPayment * countMonths;
   return +sum.toFixed(2);
 }
-console.log(calculateTotalMortgage(10, 1000, 50000, 12));
