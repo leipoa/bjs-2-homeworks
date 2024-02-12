2db34ab21cea08634ecec3008aa1f7c291a5da03
Darya Vylomova <qwerty6108@gmail.com>
2024-02-12 22:47:48 +0300

    modified arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+    return arr1.every((value, index) => {
+      return value === arr2[index];
+    });
+  }
+  function getUsersNamesInAgeRange(users, gender) {
+    const filteredUsers = users.filter((user) => user.gender === gender);
+    if (filteredUsers.length === 0) {
+      return 0;
+    }
+    const totalAges = filteredUsers
+      .map((user) => user.age)
+      .reduce((a, b) => a + b, 0);
+    const averageAge = totalAges / filteredUsers.length;
+    return averageAge;
+  }
   
-}
-
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
