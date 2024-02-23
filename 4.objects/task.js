a40f66d17acc8592b803e598151e43ec8a957e60
Darya Vylomova <qwerty6108@gmail.com>
2024-02-23 19:36:17 +0300

    modified objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  };
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  Student.prototype.addMarks = function (...marksToAdd) {
+    if (!this.marks) {
+      Console.log("Студент отчислен.");
+      return;
+    }
+    this.marks.push(...marksToAdd);
+  };
+  Student.prototype.getAverage = function () {
+    if (!this.marks || !this.marks.lenght) {
+      return 0;
+    }
+    let sum = this.marks.reduse((acc, mark) => acc + mark, 0);
+    return sum / marks.lenght;
+  };
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  };
   
-}
-
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
